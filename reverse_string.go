2fc1eaeaa7fd35b46489df57aec461ccedfcad02
sungatuly22 <sungatsson22@gmail.com>
2022-06-29 16:30:59 +0600

    4th homework(first commit)

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	r := []rune(input)
+	for i := len(r) - 1; i >= 0; i-- {
+		output += string(r[i])
+	}
 	return output
 }
